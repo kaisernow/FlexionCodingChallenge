2f5f7988dfdb23f3ca42a08ecdd4e89612a34db2
kaisernow <kaisernow@gmail.com>
2020-08-13 13:00:04 -0400

    Fixes the issue

--- client/conversion-app/src/app/modules/conversion/services/conversion.service.ts
+++ client/conversion-app/src/app/modules/conversion/services/conversion.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { of, Observable } from 'rxjs';
@@ -8,7 +8,7 @@ import { IConvert } from '../../../shared/interfaces/conversion.interfaces';
   providedIn: 'root'
 })
 export class ConversionService {
-  private baseUrl = process.env.NODE_ENV === 'production' ? `http://demoebsga-env.eba-b28bc2gd.us-east-1.elasticbeanstalk.com/api/v1/` : `http://localhost:3000/api/v1/`;
+  private baseUrl = isDevMode() ? "http://demoebsga-env.eba-b28bc2gd.us-east-1.elasticbeanstalk.com/api/v1/" : "http://localhost:3000/api/v1/";
 
   constructor(private readonly http: HttpClient) {}
 
